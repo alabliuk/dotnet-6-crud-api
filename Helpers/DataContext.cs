22abff495d3f14390a2f704ca4dcd08f20661570
andre.leme <andre.leme@gertec.com.br>
2023-06-23 14:06:07 -0300

    add migrations

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -1,22 +1,23 @@
-namespace WebApi.Helpers;
-
 using Microsoft.EntityFrameworkCore;
 using WebApi.Entities;
 
+namespace WebApi.Helpers;
+
 public class DataContext : DbContext
 {
-    protected readonly IConfiguration Configuration;
+	protected readonly IConfiguration Configuration;
 
-    public DataContext(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+	public DataContext(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        // in memory database used for simplicity, change to a real db for production applications
-        options.UseInMemoryDatabase("TestDb");
-    }
+	protected override void OnConfiguring(DbContextOptionsBuilder options)
+	{
+		// connect to mysql with connection string from app settings
+		var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+		options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+	}
 
-    public DbSet<User> Users { get; set; }
+	public DbSet<User> Users { get; set; }
 }
