7841019b75c4a49119587c3801bf7f6159c32ef2
andre.leme <andre.leme@gertec.com.br>
2023-06-23 11:49:46 -0300

    add swagger

--- Program.cs
+++ Program.cs
@@ -3,42 +3,47 @@ using WebApi.Helpers;
 using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // add services to DI container
 {
-    var services = builder.Services;
-    var env = builder.Environment;
- 
-    services.AddDbContext<DataContext>();
-    services.AddCors();
-    services.AddControllers().AddJsonOptions(x =>
-    {
-        // serialize enums as strings in api responses (e.g. Role)
-        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+	var services = builder.Services;
+	var env = builder.Environment;
 
-        // ignore omitted parameters on models to enable optional params (e.g. User update)
-        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-    });
-    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+	services.AddDbContext<DataContext>();
+	services.AddCors();
+	services.AddControllers().AddJsonOptions(x =>
+	{
+		// serialize enums as strings in api responses (e.g. Role)
+		x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 
-    // configure DI for application services
-    services.AddScoped<IUserService, UserService>();
+		// ignore omitted parameters on models to enable optional params (e.g. User update)
+		x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+	});
+	services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+	// configure DI for application services
+	services.AddScoped<IUserService, UserService>();
 }
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // configure HTTP request pipeline
 {
-    // global cors policy
-    app.UseCors(x => x
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader());
+	// global cors policy
+	app.UseCors(x => x
+		.AllowAnyOrigin()
+		.AllowAnyMethod()
+		.AllowAnyHeader());
 
-    // global error handler
-    app.UseMiddleware<ErrorHandlerMiddleware>();
+	// global error handler
+	app.UseMiddleware<ErrorHandlerMiddleware>();
 
-    app.MapControllers();
+	app.MapControllers();
 }
 
 app.Run("http://localhost:4000");
